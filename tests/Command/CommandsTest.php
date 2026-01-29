6d72ff55616516796b1fbe476d7c28ce245b6390
Imre Nagy <nagyimre.info@gmail.com>
2026-01-29 14:07:21 +0000

    commands tested!

---
+++ tests/Command/CommandsTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rovers\Tests\Command;
+
+use PHPUnit\Framework\TestCase;
+use Rovers\Command\CommandFactory;
+use Rovers\Command\ForwardCommand;
+use Rovers\Orientation;
+use Rovers\Rover;
+use Rovers\Terrain;
+
+final class CommandsTest extends TestCase
+{
+    public function testForwardCommandMovesRover(): void
+    {
+        $terrain = new Terrain(5, 5);
+        $rover = new Rover(2, 2, Orientation::north(), $terrain);
+        $cmd = new ForwardCommand();
+        $cmd->execute($rover);
+        $this->assertSame(2, $rover->x());
+        $this->assertSame(3, $rover->y());
+    }
+
+    public function testCommandFactoryCreatesCommands(): void
+    {
+        $this->assertSame('L', CommandFactory::create('L')->getLetter());
+        $this->assertSame('R', CommandFactory::create('R')->getLetter());
+        $this->assertSame('F', CommandFactory::create('F')->getLetter());
+    }
+
+    public function testCommandFactoryThrowsExceptionForUnknownCommand(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Unknown command: X');
+        CommandFactory::create('X');
+    }
+
+    
+}
